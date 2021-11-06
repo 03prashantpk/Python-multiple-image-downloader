baf5f505a123c55c8c990343586cea66f0226eb3
prashant kumar <prashantmanwan@gmail.com>
2021-11-06 21:04:40 +0530

    Created Readme.md

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+######  Instruction ###########
+
+'''
+Just Run: PS: py main.py
+It will start downloading files and will remain automatically.
+'''
+
 import main_script
 main_script.get_images_data()
 

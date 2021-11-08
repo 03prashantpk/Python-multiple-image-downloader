2c9d02b6d801387e319b6b8acd31c58bd94e6082
prashant kumar <prashantmanwan@gmail.com>
2021-11-08 18:25:21 +0530

    open downlod folder

--- Run_Image_Downloader.bat
+++ Run_Image_Downloader.bat
@@ -1,4 +1,11 @@
 
 @echo off
 python main.py %*
+
+Echo ===================================
+echo    Show Downloaded Files
+Echo ===================================
+
 pause
+set mypath=%cd%
+%SystemRoot%\explorer.exe %mypath%\images
